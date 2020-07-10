431c426e946c54831b3f6959bf9e46f099839b7d
Anderson Fernandes <andersonfferreira13@hotmail.com>
2020-07-10 18:49:12 -0300

    Usabilidade do input

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SignIn from './pages/SigIn';
+// import SignIn from './pages/SigIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
     <>
-        <SignIn />
+        <SignUp />
         <GlobalStyle />
     </>
 );
