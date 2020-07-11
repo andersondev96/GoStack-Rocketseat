9ad0847f4da4ee85689937e1e81f1d01d1128eeb
Anderson Fernandes <andersonfferreira13@hotmail.com>
2020-07-11 20:16:21 -0300

    Animando Toasts

--- web/src/pages/SigIn/index.tsx
+++ web/src/pages/SigIn/index.tsx
@@ -4,7 +4,9 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
 import getValidationErrors from '../../utils/getValidateErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -23,6 +25,7 @@ const SigIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
     const { signIn } = useAuth();
+    const { addToast } = useToast();
 
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
@@ -39,7 +42,7 @@ const SigIn: React.FC = () => {
                     abortEarly: false,
                 });
 
-                signIn({
+                await signIn({
                     email: data.email,
                     password: data.password,
                 });
@@ -50,10 +53,15 @@ const SigIn: React.FC = () => {
                     formRef.current?.setErrors(errors);
                 }
 
-                // disparar um toast
+                addToast({
+                    type: 'error',
+                    title: 'Erro na autenticação',
+                    description:
+                        'Ocorreu um erro ao fazer login, cheque as credenciais.',
+                });
             }
         },
-        [signIn],
+        [signIn, addToast],
     );
     return (
         <Container>
