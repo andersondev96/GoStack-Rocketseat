a42707cc43ae07e045790fa468364c8745941bd3
Anderson Fernandes <andersonfferreira13@hotmail.com>
2020-07-11 17:57:35 -0300

    Estrutura de Toasts

--- web/src/pages/SigIn/index.tsx
+++ web/src/pages/SigIn/index.tsx
@@ -1,10 +1,10 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidateErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -22,7 +22,7 @@ interface SignInFormData {
 const SigIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const { signIn } = useContext(AuthContext);
+    const { signIn } = useAuth();
 
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
@@ -44,11 +44,13 @@ const SigIn: React.FC = () => {
                     password: data.password,
                 });
             } catch (err) {
-                console.log(err);
+                if (err instanceof Yup.ValidationError) {
+                    const errors = getValidationErrors(err);
 
-                const errors = getValidationErrors(err);
+                    formRef.current?.setErrors(errors);
+                }
 
-                formRef.current?.setErrors(errors);
+                // disparar um toast
             }
         },
         [signIn],
