14e06a8be9f162f8cf46b74a842cb15aecdec5de
AndersonUfop <andersonfferreira13@hotmail.com>
2020-07-22 21:06:40 -0300

    Template email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,8 +1,9 @@
 import { injectable, inject } from 'tsyringe';
 
-/* import AppError from '@shared/errors/AppError'; */
+import AppError from '@shared/errors/AppError';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUserRepository';
+import IUserTokensRepository from '../repositories/IUserTokenRepository';
 
 /* import User from '../infra/typeorm/entities/User'; */
 
@@ -18,13 +19,34 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
-    this.mailProvider.sendMail(
-      email,
-      'Pedido de recuperação de senha recebido.',
-    );
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const { token } = await this.userTokensRepository.generate(user.id);
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        template: 'Olá, {{name}}: {{token}}',
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
+    });
   }
 }
 
