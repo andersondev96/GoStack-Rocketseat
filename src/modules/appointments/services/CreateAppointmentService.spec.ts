7e2a56581b3b0017cfc567446ea66611fb5c0190
AndersonUfop <andersonfferreira13@hotmail.com>
2020-08-11 20:26:59 -0300

    Correcting CreateAppointmentService.spec.ts

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -25,11 +25,11 @@ describe('CreateAppointment', () => {
 
   it('should be able to create a new appointment', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2020, 4, 10, 12).getTime();
+      return new Date(2020, 9, 10, 12).getTime();
     });
 
     const appointment = await createAppointment.execute({
-      date: new Date(2020, 4, 10, 13),
+      date: new Date(2020, 9, 10, 13),
       user_id: 'user-id',
       provider_id: 'provider-id',
     });
@@ -40,10 +40,10 @@ describe('CreateAppointment', () => {
 
   it('should not be able to create two appointments on the same time', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
-      return new Date(2020, 4, 10, 10).getTime();
+      return new Date(2020, 9, 10, 10).getTime();
     });
 
-    const appointmentDate = new Date(2020, 4, 10, 11);
+    const appointmentDate = new Date(2020, 9, 10, 11);
 
     await createAppointment.execute({
       date: appointmentDate,
