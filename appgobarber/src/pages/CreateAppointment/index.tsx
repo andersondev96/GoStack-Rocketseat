af151a85d369e179fe4d905aec0ba789e4987ad4
AndersonUfop <andersonfferreira13@hotmail.com>
2020-08-16 23:02:47 -0300

    Seeking API availability

--- appgobarber/src/pages/CreateAppointment/index.tsx
+++ appgobarber/src/pages/CreateAppointment/index.tsx
@@ -34,6 +34,11 @@ export interface Provider {
   avatar_url: string;
 }
 
+interface AvailabilityItem {
+  hour: number;
+  available: boolean;
+}
+
 const CreatedAppointment: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute();
@@ -41,6 +46,7 @@ const CreatedAppointment: React.FC = () => {
 
   const routeParams = route.params as RouteParams;
 
+  const [availability, setAvailabity] = useState<AvailabilityItem[]>([]);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [providers, setProviders] = useState<Provider[]>([]);
@@ -53,6 +59,20 @@ const CreatedAppointment: React.FC = () => {
       setProviders(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    api
+      .get(`providers/${selectedProvider}/day-availability`, {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then(response => {
+        setAvailabity(response.data);
+      });
+  }, [selectedDate, selectedProvider]);
 
   const navigateBack = useCallback(() => {
     goBack();
@@ -125,7 +145,7 @@ const CreatedAppointment: React.FC = () => {
             mode="date"
             display="calendar"
             onChange={handleDateChanged}
-            textColor="#f4ede8"
+            // textColor="#f4ede8"
             value={selectedDate}
           />
         )}
